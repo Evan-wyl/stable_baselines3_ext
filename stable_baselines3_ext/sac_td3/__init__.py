f48147bd2333a0a631a4ea5c44fe13ae2898756c
wyli <evan.wy.li@gmail.com>
2024-10-22 15:44:17 +0800

    adding amp_ppo

--- stable_baselines3_ext/sac_td3/__init__.py
+++ stable_baselines3_ext/sac_td3/__init__.py
@@ -1,4 +1,4 @@
-from stable_baselines3_ext.sac_td3.policies import CnnPolicywithBN, MlpPolicywithBN, MultiInputPolicywithBN
+from stable_baselines3_ext.sac_td3.policies import CNNPolicywithBN, MlpPolicywithBN, MultiInputPolicywithBN
 from stable_baselines3_ext.sac_td3.sac import SACTD3
 
-__all__ = ["CnnPolicywithBN", "MlpPolicywithBN", "MultiInputPolicywithBN", "SACTD3"]
+__all__ = ["CNNPolicywithBN", "MlpPolicywithBN", "MultiInputPolicywithBN", "SACTD3"]
